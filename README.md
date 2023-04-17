a01a3a7f9f629b810cf16be11110670553b99da5
Jhonattan Benitez Ballesteros <jhonattan.benitez@hotmail.com>
2023-04-17 18:43:15 -0500

    a11y finish

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# a11llymodalslider
- 
+This code is a JavaScript program that implements a simple image slider with pagination and a modal window that displays a larger version of the selected image. The code allows the user to cycle through a set of profile images by clicking on arrow buttons or dots, and also allows the user to select a profile card from a grid of cards. When the user selects a card, the modal window pops up and shows the corresponding profile image. The code also includes the feature to trap the tab key within the same elements of the active slide, which allows keyboard-only users to navigate the modal window and slider without losing focus.
+
+Here's how the code works:
+
+First, it declares and initializes variables that represent the DOM elements used by the program. These variables are used later to attach event listeners and modify the styles and attributes of the elements.
+
+Then, it defines two helper functions, setSlidesDataIndex() and createPaginationBullets(). setSlidesDataIndex() sets the data-index attribute for each slide element to its index value in the slides NodeList. This attribute is used to keep track of the current slide index when the user clicks on arrow buttons or dots. createPaginationBullets() creates a set of pagination dots based on the number of slides, and attaches click and keydown event listeners to each dot to update the current slide index and style.
+
+Next, it defines the arrows() function that takes the current slide index as an argument, and attaches click and keydown event listeners to the arrow buttons to update the current slide index and style accordingly.
+
+Then, it attaches a click event listener to each profile card in the grid, which sets the current slide index to the index of the clicked card, updates the pagination dots and arrow buttons accordingly, attaches the arrows() function to the arrow buttons, and shows the modal window by removing the 'c-modal--hidden' class from its classList.
+
+The updateSlide() function is responsible for updating the style and attributes of each slide and its associated elements when the user navigates the slider or clicks on a card. It takes the current slide index as an argument, sets the data-index attribute for each slide element again, adds the 'c-slider__active' class to the active slide, removes it from the inactive slides, and sets the aria-hidden, tabindex, and focus styles accordingly. It also calls the trapTabKey() function to trap the tab key within the same elements of the active slide.
+
+The updateBullets() function is responsible for updating the style and attributes of the pagination dots when the user navigates the slider or clicks on a card. It takes the current slide index as an argument, and adds the 'c-slider__pagination-bullet--active' class and sets the aria-selected attribute to true for the active dot, and removes it and sets it to false for the inactive dots.
+
+The setCurrentSlideIndex() function is responsible for setting the 'c-slider__active' class on the current slide element and removing it from the others when the user clicks on a card.
+
+Finally, the trapTabKey() function is responsible for trapping the tab key within the same elements of the active slide when the user navigates the modal window using the keyboard. It first selects all the focusable elements within the active slide and the slide elements of the modal window, and assigns them a tabindex of 0. Then, it adds a focus event listener to each focusable element that checks if it's the last or first element in the list, and if so, moves the focus back to the first or last element in the list accordingly.
--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,15 @@
 // Get the modal element
 const modal = document.querySelector('.c-modal');
-console.log(modal);
 
 // Get the button that opens the modal
 const btn = document.getElementById('open-modal');
-console.log(btn);
 
 // Get the Button element that closes the modal
 const closeBtn = document.querySelector('.c-modal__close');
-console.log(closeBtn);
 
 // When the user clicks on the button, open the modal
 btn.addEventListener('click', () => {
-  modal.classList.remove('c-modal--hidden')
+  modal.classList.remove('c-modal--hidden');
 });
 
 // When the user clicks on <span> (x), close the modal
@@ -23,10 +20,10 @@ closeBtn.addEventListener('click', () => {
 
 // When the user clicks anywhere outside of the modal, close it
 modal.addEventListener('click', (event) => {
-    if (event.target === modal) {
-        modal.classList.add('c-modal--hidden');
-    }
-})
+  if (event.target === modal) {
+    modal.classList.add('c-modal--hidden');
+  }
+});
 
 // When the user press Esc
 
@@ -35,3 +32,172 @@ document.addEventListener('keydown', function (event) {
     modal.classList.add('c-modal--hidden');
   }
 });
+
+// Get the slides
+const slides = document.querySelectorAll('.c-slider__slide');
+const arrowLeft = document.querySelector('.c-slider__arrow--left');
+const arrowRight = document.querySelector('.c-slider__arrow--right');
+const img = document.querySelector('.c-profile__img');
+const profileName = document.querySelector('.c-profile__name');
+const description = document.querySelector('.c-slider__desc');
+const cards = document.querySelectorAll('.c-profile__card')
+
+// Get the current slide index
+function arrows(currentIndex) {
+    arrowRight.addEventListener('click', () => {
+      if (currentIndex < slides.length - 1) {
+        console.log('right');
+        currentIndex++;
+        updateSlide(currentIndex);
+        updateBullets(currentIndex);
+      }
+    });
+    arrowLeft.addEventListener('click', () => {
+      if (currentIndex > 0) {
+        currentIndex--;
+        updateSlide(currentIndex);
+        updateBullets(currentIndex);
+      }
+    });
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'ArrowRight' && currentIndex < slides.length - 1) {
+        console.log('right');
+        currentIndex++;
+        updateSlide(currentIndex);
+        updateBullets(currentIndex);
+      }
+      if (event.key === 'ArrowLeft' && currentIndex > 0) {
+        currentIndex--;
+        updateSlide(currentIndex);
+        updateBullets(currentIndex);
+      }
+    });
+  }
+
+function setSlidesDataIndex() {
+  slides.forEach((slide, index) => {
+    slide.setAttribute('data-index', index);
+  });
+}
+
+setSlidesDataIndex()
+
+cards.forEach((card, index) => {
+    card.addEventListener('click', () => {
+        setCurrentSlideIndex(index)
+        updateBullets(index)
+        arrows(index)
+        modal.classList.remove('c-modal--hidden');
+    })
+})
+
+function updateSlide(i) {
+  slides.forEach((slide, index) => {
+    slide.setAttribute('data-index', index);
+    if (index === i) {
+      slide.classList.add('c-slider__active');
+      slide.setAttribute('aria-hidden', 'false');
+      img.setAttribute('tabindex', '0');
+      profileName.setAttribute('tabindex', '0');
+      description.setAttribute('tabindex', '0');
+      trapTabKey(slide)
+    } else {
+      slide.classList.remove('c-slider__active');
+      slide.setAttribute('aria-hidden', 'true');
+      img.setAttribute('tabindex', '-1');
+      profileName.setAttribute('tabindex', '-1');
+      description.setAttribute('tabindex', '-1');
+    }
+  });
+}
+
+const pagination = document.querySelector('.c-slider__pagination');
+function createPaginationBullets() {
+  for (let i = 0; i < slides.length; i++) {
+    const bullet = document.createElement('button');
+    bullet.classList.add('c-slider__pagination-bullet');
+    bullet.dataset.index = i;
+    bullet.setAttribute('aria-selected', 'false');
+    bullet.setAttribute('tabindex', '0');
+    bullet.addEventListener('click', () => {
+       updateSlide(parseInt(bullet.dataset.index))
+       updateBullets(parseInt(bullet.dataset.index))
+    });
+    bullet.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter' || event.key === 'Space') {
+        updateSlide(parseInt(bullet.dataset.index))
+        updateBullets(parseInt(bullet.dataset.index))
+      }
+    });
+    pagination.appendChild(bullet);
+  }
+}
+if (pagination) {
+  createPaginationBullets();
+  addAriaLabelToBulletPoints()
+}
+function addAriaLabelToBulletPoints () {
+    const profileNames = document.querySelectorAll('.c-profile__name');
+    const bullets = document.querySelectorAll('.c-slider__pagination-bullet');
+    bullets.forEach((bullet, index) => {
+        bullet.setAttribute('aria-label', `Go to ${profileNames[index].innerText} profile`);
+    })
+}
+function updateBullets(index) {
+    const bullets = document.querySelectorAll('.c-slider__pagination-bullet');
+    bullets.forEach((bullet, i) => {
+      if (i === index) {
+        bullet.classList.add('c-slider__pagination-bullet--active');
+        bullet.setAttribute('aria-selected', 'true');
+      } else {
+        bullet.classList.remove('c-slider__pagination-bullet--active');
+        bullet.setAttribute('aria-selected', 'false');
+      }
+    });
+  }
+function setCurrentSlideIndex(index) {
+    slides.forEach((slide) => {
+        const dataIndex = parseInt(slide.dataset.index)
+        if (dataIndex !== index) {
+            slide.classList.remove('c-slider__active')
+        } else {
+            slide.classList.add('c-slider__active')
+        }
+    })
+}
+
+function trapTabKey(slide) {
+    const focusableActiveSlideEls = slide.querySelectorAll('a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select');
+    const focusableSlideElements = modal.querySelectorAll('button')
+    const focusableContent = [...focusableActiveSlideEls, ...focusableSlideElements]
+    focusableContent.forEach(el => {
+        el.setAttribute('tabindex', '0')
+        el.addEventListener('focus', (e) => { 
+            console.log(e)
+        })
+    })
+
+    const firstFocusableEl = focusableContent[0];
+    const lastFocusableEl = focusableContent[focusableContent.length - 1];
+    console.log('fist focusable', firstFocusableEl)
+    console.log('last focusable', lastFocusableEl)
+    console.log('activeElement', document.activeElement)
+    console.log(focusableContent)
+  
+    slide.addEventListener('keydown', (e) => {
+      if (e.key === 'Tab' || e.keyCode === 9) {
+        if (e.shiftKey) {
+          if (document.activeElement === firstFocusableEl) {
+            lastFocusableEl.focus();
+            e.preventDefault();
+          }
+        } else {
+          if (document.activeElement === lastFocusableEl) {
+            firstFocusableEl.focus();
+            e.preventDefault();
+          }
+        }
+      }
+    });
+    firstFocusableEl.focus();
+  }
